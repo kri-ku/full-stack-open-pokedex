0834346ef3b1bd8a8f6aa870fdf5701105314b5b
kristiina kumila <kristiinakumila@gmail.com>
2021-11-26 15:22:29 +0200

    broken healthcheck url

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
-  const crash = data[9999999999]
+  //const crash = data[9999999999]
   if (isLoading) {
     return <LoadingSpinner />
   }
